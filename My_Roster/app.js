7536bd9298b610da996595ac5f960ab6f61b8210
Kenny <Kennypowers1987@gmail.com>
2015-09-15 17:40:06 -0600

    Built constructor and html an push mechanism for adding players to roster

---
+++ My_Roster/app.js
@@ -0,0 +1,31 @@
+﻿var playerRoster = [];
+
+function Player(name, position, number) {
+    this.playerName = name;
+    this.playerPosition = position;
+    this.playerNumber = number;
+  
+}
+
+$("#Submit").click(function () {
+    var playerName = $("playerName").val();
+    var playerPosition = $("playerPosition").val();
+    var playerNumber = $("playerNumber").val();
+
+    var player = new Player(playerName, playerPosition, playerNumber);
+
+    playerRoster.push(player);
+    displayPlayer();
+       
+
+});
+/*
+*/
+
+function displayPlayer(name,pos,num){
+    
+    for (var i = 0; i < playerRoster.length; i++) {
+        $('#roster').append('<div class="player-card"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"><span id="pName">' + playerRoster[i].name + '</span><span id="pPosition">' + PlayerRoster[i].pos + '</span><span id="pNumber">' + playerRoster[i].num + '</span></div>')
+    }
+
+}
