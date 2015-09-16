f9d2f95d4ce2fd34c52806e32ff330f0f8813ae8
Kenny <Kennypowers1987@gmail.com>
2015-09-15 18:56:08 -0600

    Stuck

--- My_Roster/app.js
+++ My_Roster/app.js
@@ -6,26 +6,26 @@ function Player(name, position, number) {
     this.playerNumber = number;
   
 }
-
-$("#Submit").click(function () {
-    var playerName = $("playerName").val();
-    var playerPosition = $("playerPosition").val();
-    var playerNumber = $("playerNumber").val();
-
-    var player = new Player(playerName, playerPosition, playerNumber);
-
-    playerRoster.push(player);
-    displayPlayer();
+$(document).ready(function () {
+    $("#submit").on('click', function () {
+        var playerName = $("playerName").val();
+        var playerPosition = $("playerPosition").val();
+        var playerNumber = $("playerNumber").val();
+        var player = new Player(playerName, playerPosition, playerNumber);
+        playerRoster.push(player);
        
+        //displayPlayer();
+    });
 
-});
 /*
-*/
 
-function displayPlayer(name,pos,num){
+
+function displayPlayer(){
     
     for (var i = 0; i < playerRoster.length; i++) {
-        $('#roster').append('<div class="player-card"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"><span id="pName">' + playerRoster[i].name + '</span><span id="pPosition">' + PlayerRoster[i].pos + '</span><span id="pNumber">' + playerRoster[i].num + '</span></div>')
+        $('#roster').append('<div class="player-card"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"><span id="pName">' + playerRoster[i].name + '</span><span id="pPosition">' + PlayerRoster[i].position + '</span><span id="pNumber">' + playerRoster[i].number + '</span></div>')
     }
 
 }
+*/
+});
