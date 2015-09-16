5382fbc0231e6b3fdb70f69b3af5429f65dc13a8
Kenny <Kennypowers1987@gmail.com>
2015-09-15 22:09:18 -0600

    Added Delete Functionality

--- My_Roster/app.js
+++ My_Roster/app.js
@@ -1,31 +1,59 @@
-﻿var playerRoster = [];
-
+﻿
 function Player(name, position, number) {
-    this.playerName = name;
-    this.playerPosition = position;
-    this.playerNumber = number;
-  
+    playerCount++;
+    this.name = name;
+    this.position = position;
+    this.number = number;
+    this.id = playerCount;
 }
+
+var playerRoster = {};
 $(document).ready(function () {
+   
+    var playerCount = 0;
+   
+
+
     $("#submit").on('click', function () {
-        var playerName = $("playerName").val();
-        var playerPosition = $("playerPosition").val();
-        var playerNumber = $("playerNumber").val();
-        var player = new Player(playerName, playerPosition, playerNumber);
-        playerRoster.push(player);
+        
+        var name = $("#playerName").val();
+        var position = $("#playerPosition").val();
+        var number = $("#playerNumber").val();
+        var player = new Player(name, position, number);
+        playerRoster[player.id] = player;
        
-        //displayPlayer();
+        $("#playerName").val('');
+        $("#playerPosition").val('');
+        $("#playerNumber").val('');
+        displayPlayer();
+        
     });
 
-/*
 
 
-function displayPlayer(){
-    
-    for (var i = 0; i < playerRoster.length; i++) {
-        $('#roster').append('<div class="player-card"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"><span id="pName">' + playerRoster[i].name + '</span><span id="pPosition">' + PlayerRoster[i].position + '</span><span id="pNumber">' + playerRoster[i].number + '</span></div>')
+
+    function displayPlayer() {
+       // debugger;
+    $("#roster").html("");
+   
+    for (i in playerRoster) {
+        $("#roster").append('<div class="player-card"> <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/"><input type="button" id="' + playerRoster[i].id + '" class="delete" value="Delete"><p id="pName">' + playerRoster[i].name + '</p><p id="pPosition">' + playerRoster[i].position + '</p><p id="pNumber">' + playerRoster[i].number + '</p></div>')
+       // $("#roster").append("Kenny");
     }
 
 }
-*/
+    $("#roster").on('click','.delete', function (e) {
+
+     //   debugger;
+        deletePlayer(this.id);
+        displayPlayer();
+
+    });
 });
+
+function deletePlayer(i){
+
+    for (i in playerRoster) if(i = playerRoster[i].id) delete playerRoster[i];
+
+
+}
